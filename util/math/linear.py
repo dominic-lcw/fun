4bf7fce170b005553487873162b54749509b885d
Dominic Leung <leungcheuk209@hotmail.com>
2024-04-07 13:30:31 +0800

    update censored regression

---
+++ util/math/linear.py
@@ -0,0 +1,17 @@
+from typing import List, Union
+
+import numpy as np
+
+def column_vector(v: Union[List, np.ndarray]) -> np.ndarray:
+    return np.array(v).reshape(-1, 1)
+
+def column_indicator(A: np.ndarray) -> np.ndarray:
+    """ A is the event in column vector """
+    return A.astype(int)
+
+def inner_product(v1: np.ndarray, v2: np.ndarray, rminf = True) -> float:
+    if rminf:
+        v1 = np.where(np.isinf(v1), 0, v1)
+        v2 = np.where(np.isinf(v2), 0, v2)
+    return np.dot(v1.T, v2)
+
